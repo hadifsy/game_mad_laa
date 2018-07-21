4e22fa869ae8c2020c5df2fe415ba1ddb4580cb2
hadifsy <41298778+hadifsy@users.noreply.github.com>
2018-07-21 21:34:09 +0300

    Add files via upload

---
+++ mad_lab.py
@@ -0,0 +1,33 @@
+# Write code for the function play_game, which takes in as inputs parts_of_speech
+# (a list of acceptable replacement words) and ml_string (a string that 
+# can contain replacement words that are found in parts_of_speech). Your play_game
+# function should return the joined list replaced, which will have the same structure
+# as ml_string, only that replacement words are swapped out with "corgi", since this
+# program cannot replace those words with user input. 
+
+parts_of_speech  = ["PLACE", "PERSON", "PLURALNOUN", "NOUN"]
+
+test_string = """This is PLACE, no NOUN named PERSON, We have so many PLURALNOUN around here."""
+
+def word_in_pos(word, parts_of_speech):
+    for pos in parts_of_speech:
+        if pos in word:
+            return pos
+    return None
+        
+def play_game(ml_string, parts_of_speech):    
+    replaced = []
+    list=test_string.split()
+    for i in list:
+        replacement=word_in_pos(i,parts_of_speech)
+        if( replacement== None):
+            replaced.append(i)
+        else:
+	    user=raw_input("enter word you replace " + replacement +":")
+            word=i.replace(replacement,user)
+            replaced.append(word)
+    return " ".join(replaced)
+            
+    
+print play_game(test_string, parts_of_speech)       
+
